cd64239558920abebc9c5a347fbc1bfb5a1c73f0
K L Arjun <arjunkl07@gmail.com>
2021-07-18 21:38:50 -0500

    Updated my contact info

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,10 +10,10 @@ function Contact() {
          <NavBar />
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
-          <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com my email </p>
-          <p className="lead">Phone : 732 874 0188 </p>
-          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
+          <p className="lead">I am Kempanahalli L Arjun and here are my contact details </p>
+          <p className="lead">Email : klarjun07@gmail.com </p>
+          <p className="lead">Phone : 256-468-7710 </p>
+          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp I will available morning 7AM to 11AM CST via slack </p>
         </Container>
       </Jumbotron>
     </div>
